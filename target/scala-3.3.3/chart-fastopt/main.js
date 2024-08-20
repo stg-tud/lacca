a97369e518fe0bbd9cdfcc278ecacc1febba3ef7
ldbach <ducbach1999@gmail.com>
2024-08-20 02:31:41 +0200

    Remove files now ignored by .gitignore

--- target/scala-3.3.3/chart-fastopt/main.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-import * as $j_chart$002e$002dLive$002dChart from "./chart.-Live-Chart.js";
-import * as $j_internal$002d3ebfae0cba70adf981029a0da5b1e4b5ab5d02c6 from "./internal-3ebfae0cba70adf981029a0da5b1e4b5ab5d02c6.js";
-$j_chart$002e$002dLive$002dChart.$s_Lchart_LiveChart__main__AT__V(new ($j_internal$002d3ebfae0cba70adf981029a0da5b1e4b5ab5d02c6.$d_T.getArrayOf().constr)([]));
-//# sourceMappingURL=main.js.map
