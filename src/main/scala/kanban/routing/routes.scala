8acf747b1ac0cce47c82dee6f4db3a3f4f6c6d92
Julian <git@julian-haas.de>
2025-03-31 15:28:23 +0200

    fix routes again...

--- src/main/scala/kanban/routing/routes.scala
+++ src/main/scala/kanban/routing/routes.scala
@@ -4,16 +4,17 @@ import com.raquo.waypoint.*
 import kanban.routing.Pages.*
 
 val appRoot = root / "app"
+val base = "/lacca/#"
 
 val routes = List(
-  Route.static(KanbanBoardPage, root / endOfSegments, basePath=Route.fragmentBasePath),
-  Route.static(KanbanBoardPage, root / "kanbanboard" / endOfSegments, basePath=Route.fragmentBasePath),
-  Route.static(LoginPage, root / "login" / endOfSegments, basePath=Route.fragmentBasePath),
+  Route.static(KanbanBoardPage, root / endOfSegments, basePath=base),
+  Route.static(KanbanBoardPage, root / "kanbanboard" / endOfSegments, basePath=base),
+  Route.static(LoginPage, root / "login" / endOfSegments, basePath=base),
   Route[ProjectDetailsPage, String](
     encode =
       projectDetailsPage => projectDetailsPage.id.getOrElse(0).toString(),
     decode = arg => ProjectDetailsPage(id = arg.toIntOption),
-    pattern = root / "projectDetails" / segment[String] / endOfSegments, basePath=Route.fragmentBasePath
+    pattern = root / "projectDetails" / segment[String] / endOfSegments, basePath=base
   ),
-  Route.static(AccountOverviewPage, root / "accountOverview" / endOfSegments, basePath=Route.fragmentBasePath)
+  Route.static(AccountOverviewPage, root / "accountOverview" / endOfSegments, basePath=base)
 )
