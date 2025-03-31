0ae0e72bd5ba3fc6d66fba4e3cb54475a14513f9
Julian <git@julian-haas.de>
2025-03-31 15:14:38 +0200

    use hash routing for github pages

--- src/main/scala/kanban/routing/routes.scala
+++ src/main/scala/kanban/routing/routes.scala
@@ -6,14 +6,14 @@ import kanban.routing.Pages.*
 val appRoot = root / "app"
 
 val routes = List(
-  Route.static(KanbanBoardPage, root / endOfSegments),
-  Route.static(KanbanBoardPage, root / "kanbanboard" / endOfSegments),
-  Route.static(LoginPage, root / "login" / endOfSegments),
+  Route.static(KanbanBoardPage, root / endOfSegments, basePath=Route.fragmentBasePath),
+  Route.static(KanbanBoardPage, root / "kanbanboard" / endOfSegments, basePath=Route.fragmentBasePath),
+  Route.static(LoginPage, root / "login" / endOfSegments, basePath=Route.fragmentBasePath),
   Route[ProjectDetailsPage, String](
     encode =
       projectDetailsPage => projectDetailsPage.id.getOrElse(0).toString(),
     decode = arg => ProjectDetailsPage(id = arg.toIntOption),
-    pattern = root / "projectDetails" / segment[String] / endOfSegments
+    pattern = root / "projectDetails" / segment[String] / endOfSegments, basePath=Route.fragmentBasePath
   ),
-  Route.static(AccountOverviewPage, root / "accountOverview" / endOfSegments)
+  Route.static(AccountOverviewPage, root / "accountOverview" / endOfSegments, basePath=Route.fragmentBasePath)
 )
